1632d637f0684f19ed97306c492874ad55e011ec
Olivier Otschudi <olivier.otschudi@gmail.com>
2023-04-22 12:07:58 +0200

    vidéo 8 terminée on commence la vidéo 9

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // $user = DB::insert('insert into users (name, email, password) values (?,?,?)', [
+    //     'julie',
+    //     'julie@email.com',
+    //     'password'
+    // ]);
+
+    // $user = DB::update("update users set email='abc@email.com' where id=2");
+
+    $user = DB::delete("delete from users where id=2");
+
+    $users = DB::select("select * from users");
+
+    dd($users);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
